43a62cbcb810b6630c877c5674c35eb13a89415a
Clisamax <99000834+Clisamax@users.noreply.github.com>
2024-09-04 01:28:02 -0300

    feat: inicio drawer rotas

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -1,9 +1,14 @@
+
+import DrawerContent from '@/Components/drawer-content';
 import { Drawer } from 'expo-router/drawer';
 
-export default function DrawerLayout(){
-	return(
-		<Drawer screenOptions={{headerShown: false,}}>
+export default function DrawerLayout() {
+  return (
+    <Drawer
+      defaultStatus="open"
+      screenOptions={{ headerShown: false, drawerStyle: { width: '75%' } }}
+      drawerContent={(props) => <DrawerContent {... props}/>}>
       <Drawer.Screen name="(tabs)" />
     </Drawer>
-	)
-};
+  );
+}
