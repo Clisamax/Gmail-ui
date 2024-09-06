9fb037caa14df31aeb33697471d3aaad790d11e2
Clisamax <99000834+Clisamax@users.noreply.github.com>
2024-09-05 23:55:19 -0300

    feat: finalizado

--- src/Components/drawer-content.tsx
+++ src/Components/drawer-content.tsx
@@ -1,17 +1,67 @@
-import { DrawerContentComponentProps } from '@react-navigation/drawer';
-import { Image, ScrollView, View } from 'react-native';
+import { Image, ScrollView, Text, View } from "react-native"
+import { DrawerContentComponentProps } from "@react-navigation/drawer"
 
-export default function DrawerContent(drawerProps: DrawerContentComponentProps) {
+import { CustomOptions } from "@/types/navigations"
+import { DrawerButton } from "@/Components/drawer_button"
+
+export function DrawerContent(drawerProps: DrawerContentComponentProps) {
   return (
     <View className="flex-1 bg-gray-600 overflow-hidden">
       <View className="mt-20 w-full border-b pb-6 border-gray-500">
-        <Image source={require('@/assets/logo.png')} className="w-28 ml-5" resizeMode="contain" />
+        <Image
+          source={require("@/assets/logo.png")}
+          className="w-28 ml-5"
+          resizeMode="contain"
+        />
       </View>
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 42 }}>
-				<View className="mt-2">
 
-				</View>
-			</ScrollView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 42 }}
+      >
+        <View className="mt-2">
+          {drawerProps.state.routes.map((route, index) => {
+            const isFocused = drawerProps.state.index === index
+            const options = drawerProps.descriptors[route.key]
+              .options as CustomOptions
+
+            if (options.title === undefined) {
+              return
+            }
+
+            const onPress = () => {
+              const event = drawerProps.navigation.emit({
+                type: "drawerItemPress", 
+                canPreventDefault: true,
+                target: route.key,
+              })
+
+              if (!isFocused && !event.defaultPrevented) {
+                drawerProps.navigation.navigate(route.name, route.params)
+              }
+            }
+
+            return (
+              <View key={route.key} className="w-full">
+                {options.sectionTitle && (
+                  <Text className="text-gray-400 text-sm font-heading uppercase ml-4 mt-6">
+                    {options.sectionTitle}
+                  </Text>
+                )}
+
+                <DrawerButton
+                  title={options.title}
+                  isFocused={isFocused}
+                  iconName={options.iconName}
+                  isDivider={options.isDivider}
+									notifications={options.notifications}
+                  onPress={onPress}
+                />
+              </View>
+            )
+          })}
+        </View>
+      </ScrollView>
     </View>
-  );
+  )
 }
---
+++ src/Components/drawer_button.tsx
@@ -0,0 +1,52 @@
+import { colors } from '@/styles/colors';
+import { MaterialIcons } from '@expo/vector-icons';
+import clsx from 'clsx';
+import { Pressable, PressableProps, Text, View } from 'react-native';
+
+export type IconNameType = keyof typeof MaterialIcons.glyphMap;
+
+interface TabBarButtonProps extends PressableProps {
+  title?: string;
+  isFocused?: boolean;
+  isDivider?: boolean;
+  iconName: IconNameType;
+  notifications?: number;
+}
+
+export function DrawerButton({
+  title = '',
+  iconName,
+  isDivider = false,
+  isFocused = true,
+  notifications,
+  ...rest
+}: TabBarButtonProps) {
+  return (
+    <Pressable
+      className={clsx('py-2 w-full', {
+        'border-b ml-10 border-gray-500': isDivider,
+      })}
+      {...rest}>
+      <View
+        className={clsx('flex-row items-center gap-4 w-full h-14 px-6 -ml-2', {
+          'bg-orange-800 rounded-r-full': isFocused,
+          '-ml-14': isDivider,
+        })}>
+        <MaterialIcons name={iconName} size={20} color={isFocused ? colors.orange[500] : colors.gray[400]} />
+        <Text
+          className={clsx('text-white font-subtitle text-base flex-1 ', {
+            'text-orange-300 rounded-r-full': isFocused,
+          })}>
+          {title}
+        </Text>
+
+        <Text
+          className={clsx('text-gray-400 text-sm font-body ', {
+            "text-orange-300": isFocused,
+          })}>
+          {notifications}
+        </Text>
+      </View>
+    </Pressable>
+  );
+}
